3e5144d6fe2af34157e46431e330ec76df70845b
xuzhengtong <intxzt@163.com>
2021-03-28 23:48:08 +0800

    solution 206 反转链表

---
+++ src/com/xzt/solution/Solution206.java
@@ -0,0 +1,30 @@
+package com.xzt.solution;
+
+public class Solution206 {
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode third = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return third;
+    }
+}
