5856ddc61bb3caed93d0cc9cb0a1e1208a4e8756
xuzhengtong <intxzt@163.com>
2021-03-28 21:39:34 +0800

    solution 283 移动零

---
+++ src/com/xzt/solution/Solution283.java
@@ -0,0 +1,11 @@
+package com.xzt.solution;
+
+public class Solution283 {
+    public void moveZeroes(int[] nums) {
+        int i = 0;
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != 0)
+                nums[i] = nums[i] + nums[j] - (nums[j] = nums[i++]);
+        }
+    }
+}
