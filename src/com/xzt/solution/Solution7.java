32d881747123d2040718eedcff8a9d5c49195379
xuzhengtong <intxzt@163.com>
2021-05-03 19:16:13 +0800

    solution 7 整数反转

---
+++ src/com/xzt/solution/Solution7.java
@@ -0,0 +1,19 @@
+package com.xzt.solution;
+
+public class Solution7 {
+    public static void main(String[] args) {
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+    }
+
+    public int reverse(int x) {
+        int result = 0;
+        while (x != 0) {
+            if (result < Integer.MIN_VALUE / 10 || result > Integer.MAX_VALUE / 10)
+                return 0;
+            result = result * 10 + x % 10;
+            x = x / 10;
+        }
+        return result;
+    }
+}
